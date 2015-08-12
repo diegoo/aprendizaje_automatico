a54703e743869a0ee7d01f6e800769507baf77ba
Diego Dell'Era <ddellera@socialmetrix.com>
2015-08-11 22:24:25 -0300

    test

---
+++ tp2/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# uso:
+# ./test.sh best_model.model input.arff output.arff
+
+MODELO=$1
+NUEVAS_INSTANCIAS=$2
+NUEVAS_INSTANCIAS_CLASIFICADAS="${NUEVAS_INSTANCIAS%%.*}_output.arff"
+
+echo "clasificando $NUEVAS_INSTANCIAS ..."
+
+java -classpath weka36.jar weka.filters.supervised.attribute.AddClassification -serialized $MODELO -classification -remove-old-class -i $NUEVAS_INSTANCIAS -o $NUEVAS_INSTANCIAS_CLASIFICADAS -c last
+
+echo "... resultados en $NUEVAS_INSTANCIAS_CLASIFICADAS"
